2d72eff096d8f009e0b26ef8a4a8f5f19360684a
Joe Barnes <barnesjd@gmail.com>
2016-02-28 13:36:16 -0600

    Can run some tests now

--- src/main/scala/com/joescii/js/Main.scala
+++ src/main/scala/com/joescii/js/Main.scala
@@ -4,17 +4,23 @@ import java.io.{InputStreamReader, BufferedReader}
 import java.net.URL
 
 import com.gargoylesoftware.htmlunit.html.HtmlPage
-import com.gargoylesoftware.htmlunit.{BrowserVersion, WebClient}
+import com.gargoylesoftware.htmlunit.{ScriptException, BrowserVersion, WebClient}
 import net.sourceforge.htmlunit.corejs.javascript.{ScriptableObject, Function => JsFunction}
 
 object Main extends App {
+  type JavaScript = (String, String) // Name, script contents
+
   def urlFor(resource:String):URL = this.getClass.getClassLoader.getResource(resource)
-  def readJs(path:String):String = {
+  def readJs(path:String):JavaScript = {
     val r = new BufferedReader(new InputStreamReader(urlFor(path).openStream()))
-    Stream.continually(r.readLine()).takeWhile(_ != null).mkString("\n")
+    (path, Stream.continually(r.readLine()).takeWhile(_ != null).mkString("\n"))
   }
 
   val jasmine = readJs("js/jasmine.js")
+  val boot = readJs("js/boot.js")
+  val htmlUnitBoot = readJs("js/htmlunit_boot.js")
+  val console = readJs("js/console.js")
+  val tests = readJs("js/tests.js")
 
   val client = new WebClient(BrowserVersion.CHROME)
   val options = client.getOptions()
@@ -26,18 +32,27 @@ object Main extends App {
   val window = client.getCurrentWindow().getTopWindow
   val page:HtmlPage = window.getEnclosedPage().asInstanceOf[HtmlPage] // asInstanceOf because ... java...
 
-  def run(js:String):Unit = {
-    val toRun = "function() {"+js+"\n};"
-    val result = page.executeJavaScript(js)
+  def run(js:JavaScript):Unit = {
+    val (name, script) = js
+    println(s"Running $name...")
+    val toRun = "function() {\n"+script+"\n};"
+    val result = page.executeJavaScript(toRun)
     val func:JsFunction = result.getJavaScriptResult().asInstanceOf[JsFunction]
 
-    page.executeJavaScriptFunctionIfPossible(
-      func,
-      window.getScriptObject().asInstanceOf[ScriptableObject],
-      Array.empty,
-      page.getDocumentElement())
+//    try {
+      page.executeJavaScriptFunctionIfPossible(
+        func,
+        window.getScriptObject().asInstanceOf[ScriptableObject],
+        Array.empty,
+        page.getDocumentElement())
+//    } catch {
+//      case e:ScriptException => println(e.getScriptSourceCode)
+//    }
   }
 
   run(jasmine)
+  run(console)
+  run(htmlUnitBoot)
+  run(tests)
 }
 
